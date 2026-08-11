37245830158ef880870667bb5c24ce599b34ff35
xoner1441 <63360371+xoner1441@users.noreply.github.com>
2026-08-11 20:25:43 +0200

    updated mod to 1.3.8

--- server/discordBot.js
+++ server/discordBot.js
@@ -239,7 +239,7 @@ function buildCommands() {
       .setDescription("Zeigt die Neuerungen einer betterUC-Version.")
       .addStringOption(option => option
         .setName("version")
-        .setDescription("Optional: zum Beispiel 1.3.7")
+        .setDescription("Optional: zum Beispiel 1.3.8")
         .setMaxLength(32)
         .setRequired(false)),
     new SlashCommandBuilder()
